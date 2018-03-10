507d84cd16dc0a8b07e0c326b2eaf59d7c7c42c0
Shepherd Kendall <s@kndl.org>
2018-03-10 07:15:52 -0500

    iterate

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -2,12 +2,13 @@
 
 OVS_VER=2.7.0
 
-sudo apt update && sudo apt upgrade
-sudo apt install automake autoconf gcc libssl-dev python-pip
+sudo apt -y update && sudo apt -y upgrade
+sudo apt -y install automake autoconf gcc libssl-dev python-pip
 sudo pip install six
 
 modprobe tun
 modprobe vxlan
 
 git clone https://github.com/openvswitch/ovs.git
+cd ovs
 git checkout v${OVS_VER}
