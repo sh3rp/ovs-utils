a4a84f4f5883e3c484f9853877c12ef2d0bebc29
Shepherd Kendall <s@kndl.org>
2018-03-10 07:40:02 -0500

    iterate

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -12,5 +12,9 @@ modprobe vxlan
 wget http://openvswitch.org/releases/openvswitch-2.9.0.tar.gz
 tar xvfz openvswitch-2.9.0.tar.gz
 cd openvswitch-2.9.0
-./configure
-./make
+configure
+make
+sudo make install
+sudo /sbin/modprobe openvswitch
+
+sudo /usr/local/share/openvswitch/scripts/ovs-ctl start
